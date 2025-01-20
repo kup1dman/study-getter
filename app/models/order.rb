623d452dc79113d662bcc4060b3e67f23f3dbb41
kup1dman <n.sinckevich1@gmail.com>
2025-01-20 21:02:36 +0300

    Start creating form for orders, rework header

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many_attached :files
+
+  SUBJECTS = {
+    1 => 'КПО',
+    2 => 'ОАиП',
+    3 => 'ЛАиГ'
+  }
+
+  TYPES = {
+    1 => 'Контрольная',
+    2 => 'Курсовая',
+    3 => 'Лабораторная'
+  }
 end
