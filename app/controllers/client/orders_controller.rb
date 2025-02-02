63592bc7932d0d51e11669ff17c6a8dea852f91e
kup1dman <n.sinckevich1@gmail.com>
2025-02-02 20:04:41 +0300

    Make creation of orders

--- app/controllers/client/orders_controller.rb
+++ app/controllers/client/orders_controller.rb
@@ -2,20 +2,34 @@ module Client
   class OrdersController < ApplicationController
     before_action :authenticate_user!
     def index
-      @orders = current_user.orders.all
+      @orders = current_user.orders
     end
 
     def new
-      @order = Order.new
+      @order = current_user.orders.new
     end
 
     def create
-      @order = Order.new(order_params)
-      if @order.save
+      @order = current_user.orders.new(order_params.except(:group, :desired_executor).merge(
+        subject: OrderSubject.find_by(id: params.dig(:order, :subject)),
+        type: OrderType.find_by(id: params.dig(:order, :type))
+        )
+      )
+      current_user.desired_executor ||= User.find_by(id: params.dig(:order, :desired_executor))
+      current_user.group ||= UserGroup.find_by(id: params.dig(:order, :group))
+      if @order.save && current_user.save
         redirect_to client_orders_path
       else
         render :new, status: :unprocessable_entity
       end
+    end
+
+    private
+
+    def order_params
+      params.
+        require(:order).
+        permit(:subject, :type, :deadline, :title, :group, :desired_executor, :volume_from, :volume_to, :comment)
     end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_one :subject, class_name: "OrderSubject"
-  has_one :type, class_name: "OrderType"
+  has_one :subject, class_name: "OrderSubject", dependent: :nullify
+  has_one :type, class_name: "OrderType", dependent: :nullify
   has_many_attached :files
 
   validates :subject, presence: true
